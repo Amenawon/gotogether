9cf939c40bdd8b70e90972312bda18b85f357f06
menaesezobor <menaesezobor@gmail.com>
2025-01-26 12:59:31 +0000

    add orm config for mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config'; 
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
 import { OpenaiModule } from './openai/openai.module';
 import { FactFile } from './entity/factfile.entity';
 import { PreHolidayAction } from './entity/preholiday-action.entity';
+import typeorm from './config/ormconfig';
 
 @Module({
   imports: [
@@ -14,11 +15,19 @@ import { PreHolidayAction } from './entity/preholiday-action.entity';
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
-    database: 'test',
+    password: 'password',
+    database: 'gotogether',
     entities: [FactFile, Location, PreHolidayAction],
     synchronize: true,
     //autoLoadEntities:true
+  }), 
+  ConfigModule.forRoot({
+    isGlobal: true,
+    load: [typeorm]
+  }),
+    TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
   }),
   ConfigModule.forRoot({ isGlobal: true }),  
   OpenaiModule],
