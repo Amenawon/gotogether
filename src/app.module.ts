fad7f8df9fa5da257707d473f12d60752d8b02a0
menaesezobor <menaesezobor@gmail.com>
2025-02-25 04:06:07 +0000

    update the app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { CountryModule } from './country/country.module';
+import { VisaRequirementsModule } from './visa-requirements/visa-requirements.module';
+import { UsersModule } from './users/users.module';
+import { ItineraryModule } from './itineraries/itineraries.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config'; 
-import { OpenaiModule } from './openai/openai.module';
-import { FactFile } from './entity/factfile.entity';
-import { PreHolidayAction } from './entity/preholiday-action.entity';
-import typeorm from './config/ormconfig';
-
+import typeorm from './config/typeorm';  
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'password',
-    database: 'gotogether',
-    entities: [FactFile, Location, PreHolidayAction],
-    synchronize: true,
-    //autoLoadEntities:true
-  }), 
-  ConfigModule.forRoot({
-    isGlobal: true,
-    load: [typeorm]
-  }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
     TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
-  }),
-  ConfigModule.forRoot({ isGlobal: true }),  
-  OpenaiModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    DatabaseModule,
+    CountryModule,
+    VisaRequirementsModule,
+    ItineraryModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
