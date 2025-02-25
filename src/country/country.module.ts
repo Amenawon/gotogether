b8305f8be945f111b2192933bd5f7333f3bb5a84
menaesezobor <menaesezobor@gmail.com>
2025-02-25 04:07:31 +0000

    update country controller

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { CountryController } from './country.controller';
 import { Country } from 'src/entity/country.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CountryService } from './country.service';
-
+import { CountryService } from './country.service'; 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Country])],
-     providers: [CountryService],
-    controllers: [CountryController],
-  })
-  
+  imports: [TypeOrmModule.forFeature([Country])],
+  controllers: [CountryController],
+  providers: [CountryService],
+  exports: [TypeOrmModule.forFeature([Country])]
+})
 export class CountryModule {}
