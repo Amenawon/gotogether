14163a803f38c215904f35189aa144a55f056ffc
menaesezobor <menaesezobor@gmail.com>
2025-02-25 04:08:29 +0000

    update the users feature, controller

--- src/entity/users.entity.ts
+++ src/entity/users.entity.ts
@@ -1,28 +1,52 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-@Entity('users')
-export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column()
-  name: string;
-
-  @Column({ unique: true })
-  email: string;
-
-  @Column()
-  passwordHash: string;
-
-  @Column({ nullable: true })
-  phoneNumber: string;
-
-  @Column({ nullable: true })
-  profilePicture: string;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
+// src/users/entities/user.entity.ts
+import { 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    Column, 
+    OneToMany, 
+    CreateDateColumn, 
+    UpdateDateColumn 
+  } from 'typeorm';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Itinerary } from './itineraries.entity';
+  
+  @Entity()
+  export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ unique: true })
+    @IsEmail()
+    email: string;
+  
+    // @Column()
+    // @Exclude() // Exclude password from serialization
+    // password: string;
+  
+    @Column()
+    @IsNotEmpty()
+    name: string;
+  
+    @OneToMany(() => Itinerary, (itinerary) => itinerary.user)
+    itineraries: Itinerary[];
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    // Additional common fields
+    @Column({ nullable: true })
+    phoneNumber?: string;
+  
+    @Column({ default: false })
+    isVerified: boolean;
+  
+    @Column({ nullable: true })
+    profileImage?: string;
+  
+    constructor(partial: Partial<User>) {
+      Object.assign(this, partial);
+    }
+  }
---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+// src/users/users.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from 'src/entity/users.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService], // Export for authentication module
+})
+export class UsersModule {}
