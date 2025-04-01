cfa3a0fab864dd6a55b2bd1d78816af7600dbb05
menaesezobor <menaesezobor@gmail.com>
2025-04-01 23:56:11 +0100

    fix: remove unnecessary whitespace in country entity definition

--- src/entity/country.entity.ts
+++ src/entity/country.entity.ts
@@ -7,7 +7,8 @@ export class Country {
 
   @Column({ type: "varchar", length: 100, nullable: true })
   name: string;
-
+ 
+  
   @Column({ type: "char", length: 2, nullable: true })
   code: string; 
 }
