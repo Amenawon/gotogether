8e3109c372b6988d6912e2f4189e469d6912751e
menaesezobor <menaesezobor@gmail.com>
2025-05-25 22:26:33 +0100

    fix: uncomment Swagger configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // Swagger configuration
-  // const config = new DocumentBuilder()
-  //   .setTitle('My API')
-  //   .setDescription('API documentation for My API')
-  //   .setVersion('1.0')
-  //   // .addBearerAuth() // If using JWT authentication
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for My API')
+    .setVersion('1.0')
+    // .addBearerAuth() // If using JWT authentication
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
