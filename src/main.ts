3a04cbb75990b99addbcc91bda5b0d0b1c95ab44
vincentayorinde <vincentayorinde@yahoo.com>
2025-06-07 22:02:23 +0100

    update database.module.ts 7

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import serverless from 'serverless-http';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+const expressApp = express();
 
-  // Swagger configuration
+async function bootstrap() {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+    {
+      cors: true,
+    },
+  );
+
   const config = new DocumentBuilder()
     .setTitle('My API')
-    .setDescription('API documentation for My API')
+    .setDescription('API documentation')
     .setVersion('1.0')
     .build();
 
@@ -17,7 +26,14 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.init();
-  return app.getHttpAdapter().getInstance(); // Return the Express instance
+  return serverless(expressApp);
 }
-// Export the serverless handler
-export default serverless(bootstrap);
+
+// Export a named function as required by Vercel
+let handler;
+export default async function (req, res) {
+  if (!handler) {
+    handler = await bootstrap();
+  }
+  return handler(req, res);
+}
